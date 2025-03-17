4de83b5a5c33e4a9da9aa28448c57098ecd2bdb9
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2025-03-17 13:14:33 -0400

    open

--- src/test/java/me/ebrahimhossain/pages/LoginPage.java
+++ src/test/java/me/ebrahimhossain/pages/LoginPage.java
@@ -1,13 +1,19 @@
 package me.ebrahimhossain.pages;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import me.ebrahimhossain.utilities.CommonMethods;
+import me.ebrahimhossain.utilities.ExcelUtils;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 public class LoginPage extends CommonMethods {
 
     Page page;
-
+    ExtentTest test;
 //    private Locator email;
 //    private Locator password;
 //    private Locator loginButton;
@@ -16,11 +22,11 @@ public class LoginPage extends CommonMethods {
     Locator password;
     Locator loginButton;
 
-    public LoginPage(Page page) {
+    public LoginPage(Page page, ExtentTest test) {
         this.page = page;
-
+        this.test = test;
         this.email = page.locator("#email");
-        this.password = page.locator("#password");
+        this.password = page.locator("#password2123123112");
         this.loginButton = page.locator("//button[@type='submit']");
     }
 /*
@@ -49,28 +55,77 @@ public class LoginPage extends CommonMethods {
     }
 
 */
-    public void login(){
+
+    // Method to log a success message with ExtentReports
+    public void handlePass(String message) {
+        // Log a message with green color indicating a pass
+        test.pass("<p style=\"color:#85BC63; font-size:13px\"><b>" + message + "</b></p>");
+    }
+
+    // Method to log a success message and capture a screenshot
+    public void handlePassWithScreenshot(String message, String screenshotName) {
+        // Log a message with green color and include bold formatting
+        test.pass("<p style=\"color:#85BC63; font-size:13px\"><b>" + message + "</b></p>");
+        // Capture a full-page screenshot and save it to the specified path
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("./screenshots/" + screenshotName + ".png"))
+                .setFullPage(true));
+
+
+        // Build the full path for the screenshot file
+        String dest = System.getProperty("user.dir") + "/screenshots/" + screenshotName + ".png";
+        // Attach the screenshot to the test report
+        test.pass(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
+    }
+
+    // Method to log a failure message, capture a screenshot, and log an exception
+    public void handleFail(String message, String screenshotName) {
+        // Log a failure message with red color indicating an error
+        test.fail("<p style=\"color:#FF5353; font-size:13px\"><b>" + message + "</b></p>");
+        // Create an exception and log it in the report
+        Throwable t = new InterruptedException("Exception");
+        test.fail(t);
+        // Capture a full-page screenshot in case of failure and save it to the
+        // specified path
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("./screenshots/" + screenshotName + ".png"))
+                .setFullPage(true));
+        // Build the full path for the screenshot file
+        String dest = System.getProperty("user.dir") + "/screenshots/" + screenshotName + ".png";
+        // Attach the screenshot to the failure report
+        test.fail(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
+    }
+
+
+    public void login() throws IOException {
+        ExcelUtils excelUtils = new ExcelUtils();
+        excelUtils.ReadExcel();
         try{
+            test.info("Please enter your username");
             if(email.isVisible()){
-                email.fill("john@mail.com");
+                email.fill(excelUtils.username);
+                handlePass("You have successfully entered your username");
                 page.waitForTimeout(1000);
+                test.info("Please enter your password");
                 if(password.isVisible()){
-                    password.fill("changeme");
+                    password.fill(excelUtils.password);
+                    handlePass("You have successfully entered your password");
                     page.waitForTimeout(1000);
+                    test.info("Please click Login button");
                     if(loginButton.isVisible()){
                         loginButton.click();
                         page.waitForTimeout(3000);
+                        handlePassWithScreenshot("You have successfully logged in", "login_pass");
                     }else{
-                        System.out.println("Login Button is not locateable, please check the error message");
+                        handleFail("Login Button is not locateable, please check the error message", "login_fail");
+
                     }
                 }else{
-                    System.out.println("Password Button is not locateable, please check the error message");
+                    handleFail("Password Button is not locateable, please check the error message", "password_fail");
                 }
             }else{
-                System.out.println("Email is not locateable, please check the error message");
+                handleFail("Email box is not visible, please check the error message", "email_fail");
             }
         } catch (Exception e) {
-            System.out.println("Something went wrong");
+            handleFail("Something went wrong, please check the error message", "login_page_error");
         }
     }
 
--- src/test/java/me/ebrahimhossain/tests/LoginTest.java
+++ src/test/java/me/ebrahimhossain/tests/LoginTest.java
@@ -1,10 +1,19 @@
 package me.ebrahimhossain.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import me.ebrahimhossain.basedriver.BaseDriver;
 import me.ebrahimhossain.pages.LoginPage;
+import me.ebrahimhossain.utilities.ExtentFactory;
 import org.testng.annotations.*;
 
+import java.io.IOException;
+
 public class LoginTest extends BaseDriver {
+
+    ExtentReports report;
+    ExtentTest parentTest;
+    ExtentTest childTest;
 
     @BeforeClass
     @Parameters({"url","browserName","headless"})
@@ -14,19 +23,25 @@ public class LoginTest extends BaseDriver {
 //        System.out.println("Opening URL: " + url);
 //        System.out.println("Browser: " + browserName);
 //        System.out.println("Headless: " + headless);
+        report = ExtentFactory.getInstance();
+        parentTest = report.createTest("<p style=\"color:#FF6000; font-size:20px\"><b>E-Commerce System</b></p>")
+                .assignAuthor("QA TEAM").assignDevice("Windows");
+
         start(browserName, headless);
         launchApplication(url);
     }
 
     @Test
-    public void loginTest(){
-        LoginPage loginPage = new LoginPage(page);
+    public void loginTest() throws IOException {
+        childTest = parentTest.createNode("<p style=\"color:#3E96E7; font-size:20px\"><b>Login</b></p>");
+        LoginPage loginPage = new LoginPage(page, childTest);
         loginPage.login();
     }
 
     @AfterClass
     public void tearDown(){
         stop();
+        report.flush();
     }
 
 }
